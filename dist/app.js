b28de4e718486780b6e26ed43f2489e46efa98af
Staszek <stasiu94pro@icload.com>
2022-08-03 11:23:52 +0200

    learn: types in practice

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,16 @@
+//basic types number, sting
 let age = 28;
 let ageAsString = "dwadzieścia osiem";
 ageAsString = "dwa";
 ageAsString = `${age}`;
 console.log(`ageAsString`, ageAsString);
+//types in functions
+const add = (v1, v2) => v1 + v2;
+//types in practice
+const input1 = document.querySelector("#input1");
+const input2 = document.querySelector("#input2");
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const sum = add(+input1.value, +input2.value);
+    console.log(sum);
+});
