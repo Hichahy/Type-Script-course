a79d68b07a8dfe5dd974d5682b8f951b7d0f5da4
Staszek <stasiu94pro@icload.com>
2022-08-03 12:10:03 +0200

    learn boolean and union type

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,36 @@ const add = (v1: number, v2: number) => v1 + v2;
 //types in practice
 const input1: HTMLInputElement = document.querySelector("#input1");
 const input2: HTMLInputElement = document.querySelector("#input2");
-const button = document.querySelector("button");
+const button = document.querySelector("#button1");
 
 button.addEventListener("click", () => {
-    const sum = add(+input1.value, +input2.value)
-    console.log(sum)
-})
+  const sum = add(+input1.value, +input2.value);
+  console.log(sum);
+});
+
+// inference type
+let age2 = 20; //nie trzeba wpisywac ze number bo inference sam wykryje ale tylko jesli zadeklarujemy
+// w funcjach zawsze powinnismy przekazywac deklaracje bo nie dziala inference gdy nie zadeklarujesz
+
+// union type
+const logAge = (age: number | string | boolean) => {
+  console.log(`hej mam ${age} lat`);
+};
+// union type pozwala wybrac wiecej wrtosci
+logAge(28);
+logAge("dwadzieścia osiem");
+logAge(true);
+
+//boolean in practice
+const buttonBoolean = document.querySelector("#button2");
+
+const calculatePrice = (orginalPrice: number, hasDiscount: boolean) => {
+ return hasDiscount ? orginalPrice * 0.8 : orginalPrice;
+};
+
+buttonBoolean.addEventListener("click", () => {
+  const orginalPrice = 40;
+  const hasDiscount: boolean =
+    new URLSearchParams(window.location.search).get("discount") === "true";
+  console.log(calculatePrice(orginalPrice, hasDiscount));
+});
